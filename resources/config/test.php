a4210546df7150ae93d659abf4ff3629dfc5dd0d
Rob Brown <rbtitotito@gmail.com>
2015-10-13 14:58:40 -0500

    Travis Fix
    Hopefully this does it.
    
    Modfied travis to run newly created test console, wired to
    test config

--- resources/config/test.php
+++ resources/config/test.php
@@ -1,6 +1,8 @@
 <?php
 
 $app['debug'] = true;
+$app['db.options']['user'] = 'postgres';
+$app['db.options']['password'] = '';
 if (isset($app['db.options']['dbname'])) {
     $db_options['dbname'] = $db_options['dbname'].'_test';
 }
